ff6c3aec116d3f00499b22e52a70fe2939a7149e
AlmostGivingUp <twentyseven862@gmail.com>
2026-06-22 00:55:45 +0800

    Inject keypresses via HIDProcessor with centralised InpEng

---
+++ Injectors/windows/windows_injector.py
@@ -0,0 +1,163 @@
+from Injectors.input_engine import InpEng, get_mapping
+from Injectors.windows.windows_input_sender import InputSender
+from Constants.windows_const import VK, SCROLL
+
+BUTTON_MASKS = {
+    "BUTTON 1": InpEng.BUTTON1,
+    "BUTTON 2": InpEng.BUTTON2,
+    "BUTTON 3": InpEng.BUTTON3,
+    "BUTTON 4": InpEng.BUTTON4,
+}
+
+
+class HIDProcessor:
+
+    def __init__(self):
+        self.input_engine = InpEng()
+
+    def process_hid_report(self, data: list):
+        """
+        Main entry point.
+        Called whenever a HID report arrives.
+        """
+        key_map, mouse_map = get_mapping()
+        self.input_engine.update_data(data)
+
+        if self.input_engine.check_mouse_on():
+            # Handle scrolling/cursor/mouse-related keyboard presses
+            self.update_mouse(mouse_map)
+            self.handle_cursor()
+            self.handle_scroll()
+        else:
+            self.update_keyboard(key_map)
+            self.handle_joystick_keyboard(key_map)
+
+    #------------------KeyBoard Mode--------------------#
+    def update_keyboard(self, key_map):
+        """
+        Mapping keyboard-related keypress  
+        """
+        # getting updated hardware states 
+        for button, mask in BUTTON_MASKS.items():
+            self.input_engine.update_keyboard_key(
+                button,
+                mask
+            )
+        for button in self.input_engine.keyboard.pressed:
+            mapped_key = key_map[button]
+            InputSender.press_key(VK[mapped_key])
+
+        for button in self.input_engine.keyboard.released:
+            mapped_key = key_map[button]
+            InputSender.release_key(VK[mapped_key])
+
+        self.input_engine.keyboard.update()
+
+    #-------------------Mouse Mode-------------------#
+    def update_mouse(self, mouse_map):
+        """
+        Mapping mouse-related keypress 
+        """
+        for button, mask in BUTTON_MASKS.items():
+            self.input_engine.update_mouse_key(
+                button,
+                mask
+            )
+
+        for button in self.input_engine.mouse.pressed:
+            mapped_key = mouse_map[button]
+            self._mouse_press(mapped_key)
+
+        for button in self.input_engine.mouse.released:
+            mapped_key = mouse_map[button]
+            self._mouse_release(mapped_key)
+
+        self.input_engine.mouse.update()
+
+
+    def _mouse_press(self, mapped_key):
+        """
+        Handling clicks and normal key presses
+        """
+        if mapped_key == "LEFT":
+            InputSender.left_click_press()
+
+        elif mapped_key == "RIGHT":
+            InputSender.right_click_press()
+
+        else:
+            InputSender.press_key(VK[mapped_key])
+
+    def _mouse_release(self, mapped_key):
+        """
+        Handling clicks and normal key release
+        """
+        if mapped_key == "LEFT":
+            InputSender.left_click_release()
+
+        elif mapped_key == "RIGHT":
+            InputSender.right_click_release()
+
+        else:
+            InputSender.release_key(VK[mapped_key])
+
+
+    #-------------------Cursor-------------------#
+    def handle_cursor(self):
+        """
+        Move cursor 
+        """
+        dx, dy = self.input_engine.get_cursor_delta()
+        if dx != 0 or dy != 0: # Don't move if no reads from both
+            InputSender.move_cursor(dx, dy)
+
+    #-------------------Scroll-------------------#
+    def handle_scroll(self):
+        """
+        Scroll page horizontally/vertically 
+        """
+        if not self.input_engine.check_scroll_on():
+            return
+        dx, dy = self.input_engine.get_cursor_delta()
+        if dx:
+            InputSender.h_scroll(dx * SCROLL)
+
+        if dy:
+            InputSender.v_scroll(dy * SCROLL)
+
+    #-------------------Joystick Keyboard-------------------#
+
+    def handle_joystick_keyboard(self, key_map):
+        """
+        Handling mapping joystick to keyboard key presses 
+        """
+        x = self.input_engine.joystick_x
+        y = self.input_engine.joystick_y
+
+        # X Axis
+        left_key = key_map["JOYSTICK X-AXIS LEFT"]
+        right_key = key_map["JOYSTICK X-AXIS RIGHT"]
+
+        if x > 0:
+            InputSender.release_key(VK[left_key])
+            InputSender.press_key(VK[right_key])
+        elif x <0:
+            InputSender.release_key(VK[right_key])
+            InputSender.press_key(VK[left_key])
+        else: 
+            InputSender.release_key(VK[left_key])
+            InputSender.release_key(VK[right_key])
+
+        # Y Axis
+        up_key = key_map["JOYSTICK Y-AXIS UP"]
+        down_key = key_map["JOYSTICK Y-AXIS DOWN"]
+
+        if y > 0:
+            InputSender.release_key(VK[down_key])
+            InputSender.press_key(VK[up_key])
+        elif y < 0:
+            InputSender.release_key(VK[up_key])
+            InputSender.press_key(VK[down_key])
+        else:
+            InputSender.release_key(VK[up_key])
+            InputSender.release_key(VK[down_key])
